a0b68fbe6de78f5d65ce1416c6d74eedef7e8648
tibrook <guillouet.alexandre35@gmail.com>
2024-05-10 09:55:11 +0200

    readme enhancement

--- src/main/java/com/chatop/rental/exception/AuthenticationException.java
+++ src/main/java/com/chatop/rental/exception/AuthenticationException.java
@@ -8,8 +8,6 @@ public class AuthenticationException extends RuntimeException{
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private String message;
-
 	public AuthenticationException(String message) {
 		super(message);
 	}
